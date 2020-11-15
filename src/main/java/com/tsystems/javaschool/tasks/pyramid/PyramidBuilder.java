6730e878fa0bf506073bb42a84bc97627f6aeb94
temlerg <56558240+temlerg@users.noreply.github.com>
2020-11-15 16:09:29 +0300

    first commit

---
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -0,0 +1,77 @@
+package com.tsystems.javaschool.tasks.pyramid;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class PyramidBuilder {
+
+    /**
+     * Builds a pyramid with sorted values (with minumum value at the top line and maximum at the bottom,
+     * from left to right). All vacant positions in the array are zeros.
+     *
+     * @param inputNumbers to be used in the pyramid
+     * @return 2d array with pyramid inside
+     * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
+     */
+    public int[][] buildPyramid(List<Integer> inputNumbers) {
+        // TODO : Implement your solution here
+        if(inputNumbers == null) throw new CannotBuildPyramidException();
+        else {
+            for (int i = 0; i < inputNumbers.size(); ++i)
+                if(inputNumbers.get(i) == null) throw new CannotBuildPyramidException();
+        }
+        boolean flag;
+        int[][] matrix;
+        long size = inputNumbers.size();
+        //is this sequence suitable
+        long count = 0;
+        int rows = 1;
+        int cols = 1;
+        while (count < size) {
+            count += rows;
+            rows++;
+            cols += 2;
+        }
+        rows--;
+        cols -= 2;
+        //is it possible to build
+        if (size == count) {
+            flag = true;
+        } else flag = false;
+
+        if (flag) {
+            List<Integer> t = inputNumbers.stream().sorted().collect(Collectors.toList());
+            //System.out.println("Число столбцов " + cols);
+            // System.out.println("Число строк " + rows);
+
+            matrix = new int[rows][cols];
+            for (int[] row : matrix) {
+                Arrays.fill(row, 0);
+            }
+
+            long center = (cols / 2);
+            count = 1;
+            long arrIdx = 0;
+
+            for (long i = 0, offset = 0; i < rows; i++, offset++, count++) {
+                long start = center - offset;
+                for (long j = 0; j < count * 2; j += 2, arrIdx++) {
+                    matrix[(int) i][(int) (start + j)] = t.get((int) arrIdx);
+                }
+            }
+
+            for (int[] a : matrix) {
+                for (long b : a)
+                    System.out.print(b + " ");
+                System.out.println();
+            }
+        }
+        else {
+            throw new CannotBuildPyramidException();
+        }
+        return matrix;
+    }
+
+
+}
---
+++ src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
@@ -0,0 +1,38 @@
+package com.tsystems.javaschool.tasks.subsequence;
+
+import java.util.List;
+
+public class Subsequence {
+
+    /**
+     * Checks if it is possible to get a sequence which is equal to the first
+     * one by removing some elements from the second one.
+     *
+     * @param x first sequence
+     * @param y second sequence
+     * @return <code>true</code> if possible, otherwise <code>false</code>
+     */
+    @SuppressWarnings("rawtypes")
+    public boolean find(List x, List y) {
+        // TODO: Implement the logic here
+        if(x == null || y == null) throw new IllegalArgumentException();
+        if (x != null && y != null) {
+            if (x.size() > y.size()) return false;
+            int n[] = new int[y.size()];
+            for (int i = 0; i < y.size(); ++i) n[i] = -1;
+
+            for (int i = 0; i < x.size(); ++i)
+                for (int j = 0; j < y.size(); ++j)
+                    if (y.get(j) == x.get(i)) n[j] = i;
+
+            int t = -1;
+            for (int i = 0; i < y.size(); ++i) {
+                if (n[i] != -1)
+                    if (n[i] >= t) t = n[i];
+                    else return false;
+            }
+            return true;
+        }
+        return true;
+    }
+}
